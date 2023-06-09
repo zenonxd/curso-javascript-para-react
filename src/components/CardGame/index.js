47282844d951906d393f106514786c5c8c060ef1
Olavo Moreira <olavomoreira99@gmail.com>
2023-06-09 16:26:57 -0300

    :tada: Creating components

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,13 @@
+import "./style.css"
+
+function CardGame() {
+    return `
+        <article class="card-game">
+            <img src="/marco-bruno/images/alura-pixel.png" alt="Logo da Alura">
+            aa
+        </article>
+    `
+}
+
+export default CardGame;
+
